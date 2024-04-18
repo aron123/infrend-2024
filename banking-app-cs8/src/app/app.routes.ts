72fc9beefdb58ca3c76e90e1f4470da86449428e
Kiss Áron <aron123dev@gmail.com>
2024-04-18 11:48:24 +0200

    Add code from week 10

--- banking-app-cs8/src/app/app.routes.ts
+++ banking-app-cs8/src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: UserListComponent
+    },
+    {
+        path: 'add-user',
+        component: UserFormComponent
+    },
+    {
+        path: 'edit-user/:id',
+        component: UserFormComponent
     }
 ];
